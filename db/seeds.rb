0438e1c2104cb360899f590812fece5a02f91cee
Corey Psoinos <coreypsoinos@gmail.com>
2016-03-20 18:00:31 -0400

    spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 company = Company.create(name: "Just the Right Piece", description: "The best second-hand furniture store around")
 
-company.categories.create([{ name: "Bedroom" }, { name: "Dining Room" }, { name: "Living Room"} ])
+company.categories.create([{ name: "Bedroom" }, { name: "Dining Room" }, { name: "Living Room" } ])
