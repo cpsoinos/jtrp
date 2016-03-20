4b1e10dc82a6c1118f341192cb8ce69e952f8b36
Corey Psoinos <coreypsoinos@gmail.com>
2016-03-20 18:01:05 -0400

    remove commented code and whitespace

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 company = Company.create(name: "Just the Right Piece", description: "The best second-hand furniture store around")
 
 company.categories.create([{ name: "Bedroom" }, { name: "Dining Room" }, { name: "Living Room" } ])
