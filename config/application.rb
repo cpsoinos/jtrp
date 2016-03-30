c2126442f3498a3a3febbee312507f34e30c3a0a
Corey Psoinos <coreypsoinos@gmail.com>
2016-03-30 10:25:40 -0400

    move web console config item to local.rb

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,5 @@ module Jtrp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.web_console.development_only = false
   end
 end
