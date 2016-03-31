4485a5192acc8f05179cf86f19e0d5f47ea542e8
Corey Psoinos <coreypsoinos@gmail.com>
2016-03-31 14:49:17 -0400

    stylize devise sign up and add fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331132622) do
+ActiveRecord::Schema.define(version: 20160331184632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,13 @@ ActiveRecord::Schema.define(version: 20160331132622) do
     t.string   "role",                   default: "guest"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "address_1"
+    t.string   "address_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.string   "phone_ext"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
