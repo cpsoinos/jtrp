49c16a705f6d8162352b91670fb5c270e5a9079b
Corey Psoinos <coreypsoinos@gmail.com>
2016-04-04 13:54:40 -0400

    add consignment agreement to sign up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address_1, :address_2, :city, :state, :zip, :phone, :phone_ext, :role, :consignment_policy_accepted)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+end
