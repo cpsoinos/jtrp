16329f935f36dbe2e0b2bebea9fdb268fab440bc
Corey Psoinos <coreypsoinos@gmail.com>
2016-04-06 10:48:24 -0400

    use svg for barcodes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,8 @@
 class Item < ActiveRecord::Base
   has_secure_token
-  include HasBarcode
 
   mount_uploaders :initial_photos, PhotoUploader
   mount_uploaders :listing_photos, PhotoUploader
-
-  has_barcode :barcode,
-    outputter: :svg,
-    type: :code_39,
-    value: Proc.new { |c| c.token }
 
   monetize :purchase_price_cents, allow_nil: true, numericality: {
     greater_than_or_equal_to: 0,
@@ -36,17 +30,16 @@ class Item < ActiveRecord::Base
   scope :active, -> { where(status: "active") }
   scope :sold, -> { where(status: "sold") }
 
-  # def barcode
-  #   require 'barby'
-  #   require 'barby/barcode/code_128'
-  #   require 'barby/outputter/png_outputter'
-  #   require 'barby/outputter/html_outputter'
-  #
-  #   barcode = Barby::Code128B.new(token)
-  #   # Barby::HtmlOutputter.new(barcode).to_html
-  #   blob = Barby::PngOutputter.new(barcode).to_png #Raw PNG data
-  #   File.open('barcode.png', 'wb') { |f| f.write blob }
-  # end
+  def barcode
+    require 'barby'
+    require 'barby/barcode/code_128'
+    require 'barby/outputter/png_outputter'
+    require 'barby/outputter/html_outputter'
+    require 'barby/outputter/cairo_outputter'
+
+    barcode = Barby::Code128B.new(token)
+    Barby::CairoOutputter.new(barcode).to_svg
+  end
 
   def active?
     status == "active"
