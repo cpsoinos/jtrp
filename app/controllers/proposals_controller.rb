5aa1f0ca6227fc6de9ed4c165a57cd90c63bd2e8
Corey Psoinos <coreypsoinos@gmail.com>
2016-08-20 13:43:57 -0400

    move emails to job

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -39,13 +39,13 @@ class ProposalsController < ApplicationController
 
   def send_email
     @proposal = Proposal.find(params[:proposal_id])
-    TransactionalEmailer.new(@proposal, current_user).send(params[:note])
+    TransactionalEmailJob.perform_later(@proposal, current_user, params[:note])
     redirect_to :back, notice: "Email sent to client!"
   end
 
   def notify_response
     @proposal = Proposal.find(params[:proposal_id])
-    TransactionalEmailer.new(@proposal, @proposal.account.primary_contact).send_notification(params[:note])
+    NotificationEmailJob.perform_later(@proposal, @proposal.account.primary_contact, params[:note])
     redirect_to :back, notice: "Thank you! We've been notified of your responses."
   end
 
