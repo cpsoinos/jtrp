3c58e8f9100308ce61f1db1a95e7c82356b1f551
Corey Psoinos <coreypsoinos@gmail.com>
2016-09-18 14:01:42 -0400

    remove unused yard sale / estate sale queries, formatting

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,9 +2,7 @@ class AccountsController < ApplicationController
   before_filter :require_internal
 
   def index
-    @accounts = Account.includes(:primary_contact, jobs: { proposals: :agreements}).all.order(:account_number)
-    @yard_sale = Account.yard_sale
-    @estate_sale = Account.estate_sale
+    @accounts = Account.includes(:primary_contact, jobs: { proposals: :agreements }).all.order(:account_number)
   end
 
   def show
