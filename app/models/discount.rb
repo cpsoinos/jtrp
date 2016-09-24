55c27c2b0bfbd4500ccf0e5f8faa79adb823fd62
Corey Psoinos <coreypsoinos@gmail.com>
2016-09-24 15:10:43 -0400

    validate presence of item once again

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -6,11 +6,10 @@ class Discount < ActiveRecord::Base
     less_than_or_equal_to: 0
   }
 
-  validates_presence_of :order
+  validates_presence_of :order, :item
 
   def apply_to_item
     return if applied?
-    return if item.nil?
     # this will handle marking as sold in addition to applying discount
     ItemUpdater.new(item).update(sale_price_cents: (item.listing_price_cents + calculate_discount), sold_at: order.updated_at)
     self.applied = true
