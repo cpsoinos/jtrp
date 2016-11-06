bf2657c2b1dce01b0e15a132ef6548933bf8e4ff
Corey Psoinos <coreypsoinos@gmail.com>
2016-11-06 11:00:05 -0500

    add account id to agreement

--- db/migrate/20161103234824_add_account_to_statements.rb
+++ db/migrate/20161103234824_add_account_to_statements.rb
@@ -1,5 +1,11 @@
 class AddAccountToStatements < ActiveRecord::Migration
   def change
     add_reference :statements, :account, index: true, foreign_key: true
+    Statement.reset_column_information
+
+    Statement.all.map do |statement|
+      statement.account_id = statement.agreement.account.id
+      statement.save
+    end
   end
 end
