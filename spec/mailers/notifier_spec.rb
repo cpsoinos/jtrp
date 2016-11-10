da0acc124500fc2fca100a98a6165e763336744d
Corey Psoinos <coreypsoinos@gmail.com>
2016-11-10 08:58:46 -0500

    allow custom recipient address

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -3,6 +3,7 @@ describe Notifier do
   let(:user) { build_stubbed(:internal_user) }
 
   context "daily summary email" do
+
     it "sends a daily summary email" do
       expect {
         Notifier.send_daily_summary_email(user).deliver_now
@@ -10,6 +11,7 @@ describe Notifier do
         ActionMailer::Base.deliveries.count
       }.by(1)
     end
+
   end
 
 end
