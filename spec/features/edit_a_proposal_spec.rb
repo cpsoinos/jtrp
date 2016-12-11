83e307daf3f5dab1df0ac92953351afad560a4de
Corey Psoinos <coreypsoinos@gmail.com>
2016-12-10 20:29:01 -0500

    add more wait_for_ajax on failing tests

--- spec/features/edit_a_proposal_spec.rb
+++ spec/features/edit_a_proposal_spec.rb
@@ -37,6 +37,7 @@ feature "edit a proposal" do
         visit account_job_proposal_details_path(account, job, proposal)
         expect(item.will_purchase?).to be_falsey
         find(:css, "#item_#{item.id}_will_purchase", visible: false).trigger("click")
+        wait_for_ajax
         fill_in("item_purchase_price", with: 50.55)
         click_on("Save")
 
@@ -50,6 +51,7 @@ feature "edit a proposal" do
         visit account_job_proposal_details_path(account, job, proposal)
         expect(item.will_consign?).to be_falsey
         find(:css, "#item_#{item.id}_will_consign", visible: false).trigger("click")
+        wait_for_ajax
         fill_in("item_consignment_rate", with: 45)
         fill_in("item_consignment_term", with: 90)
         fill_in("item_listing_price", with: 88.89)
@@ -70,8 +72,10 @@ feature "edit a proposal" do
         expect(item.will_consign?).to be_falsey
         expect(item.will_purchase?).to be_falsey
         find(:css, "#item_#{item.id}_will_purchase", visible: false).trigger("click")
-        fill_in("item_purchase_price", with: 50.55)
+        wait_for_ajax
         find(:css, "#item_#{item.id}_will_consign", visible: false).trigger("click")
+        wait_for_ajax
+        fill_in("item_purchase_price", with: 50.55)
         fill_in("item_consignment_rate", with: 45)
         fill_in("item_listing_price", with: 88.89)
         fill_in("item_minimum_sale_price", with: 67.55)
