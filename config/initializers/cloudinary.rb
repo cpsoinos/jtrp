692a75cabbd6f557ffe7380d862e4ccddb21e3f1
Corey Psoinos <coreypsoinos@gmail.com>
2017-01-22 05:52:29 -0500

    restrict some features to stg/prod and remove static img logs

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -2,9 +2,12 @@ Cloudinary.config do |config|
   config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
   config.api_key = ENV['CLOUDINARY_API_KEY']
   config.api_secret = ENV['CLOUDINARY_API_SECRET']
-  unless Rails.env.test?
+
+  if Rails.env.in? %w(staging production)
     config.enhance_image_tag = true
     config.static_image_support = true
     config.secure = true
+    config.cdn_subdomain = true
   end
+
 end
