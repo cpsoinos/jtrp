25caa5a4ccfab1ac374e3e05dcfe0051ceabb4c3
Corey Psoinos <coreypsoinos@gmail.com>
2017-01-29 08:00:51 -0500

    add security around receiving webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,6 @@
 class WebhooksController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token, :find_company, :find_categories, :meta_tags
+  before_filter :verify_webhook_auth_header
 
   def receive
     if request.headers['Content-Type'] == 'application/json'
@@ -11,6 +12,14 @@ class WebhooksController < ApplicationController
 
     WebhookProcessor.new(data).process
     head :ok
+  end
+
+  protected
+
+  def verify_webhook_auth_header
+    unless request.headers['X-Clover-Auth Code'] == ENV['CLOVER_WEBHOOK_AUTH_CODE']
+      head :bad_request
+    end
   end
 
 end
