42451b23fa9b3ff8894bc8718998c1b0b1d44569
Corey Psoinos <coreypsoinos@gmail.com>
2017-02-20 22:18:15 -0500

    include custom message in validation checks

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -5,7 +5,7 @@ describe Discount do
   it { should validate_presence_of(:discountable) }
 
   subject { create(:discount, remote_id: 'abc123') }
-  it { should validate_uniqueness_of(:remote_id).allow_nil }
+  it { should validate_uniqueness_of(:remote_id).with_message("remote_id already taken").allow_nil }
 
   it { should monetize(:amount).allow_nil }
 
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,7 +6,7 @@ describe Order do
   it { should have_many(:payments) }
 
   subject { create(:order, remote_id: 'abc123') }
-  it { should validate_uniqueness_of(:remote_id).allow_nil }
+  it { should validate_uniqueness_of(:remote_id).with_message("remote_id already taken").allow_nil }
 
   it { should monetize(:amount).allow_nil }
 
