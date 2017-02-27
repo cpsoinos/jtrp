e53f68550c3dfb397e8218d79863bae578a0ed93
Corey Psoinos <coreypsoinos@gmail.com>
2017-02-27 17:46:35 -0500

    pass integration name to webhook processor in spec

--- spec/services/webhook/processor_spec.rb
+++ spec/services/webhook/processor_spec.rb
@@ -1,7 +1,7 @@
 describe Webhook::Processor do
 
   let(:data) { {"key": "value"} }
-  let(:processor) { Webhook::Processor.new(data) }
+  let(:processor) { Webhook::Processor.new('clover', data) }
 
   before do
     allow(WebhookProcessorJob).to receive(:perform_later)
@@ -22,7 +22,7 @@ describe Webhook::Processor do
   it "creates webhook entries" do
     webhook = create(:webhook, :open_order)
     expect {
-      Webhook::Processor.new(webhook.data).process
+      Webhook::Processor.new('clover', webhook.data).process
     }.to change {
       WebhookEntry.count
     }.by(2)
@@ -31,7 +31,7 @@ describe Webhook::Processor do
   it "processes webhook entries" do
     allow_any_instance_of(WebhookEntry).to receive(:process)
     webhook = create(:webhook, :open_order)
-    Webhook::Processor.new(webhook.data).process
+    Webhook::Processor.new('clover', webhook.data).process
 
     webhook.webhook_entries.each do |entry|
       expect(entry).to have_received(:process)
