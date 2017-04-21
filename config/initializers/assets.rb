0c9cbeab7da6caa9bd44617f2927d8b0b55a8fe5
Corey Psoinos <coreypsoinos@gmail.com>
2017-04-21 08:11:48 -0400

    add vendor fonts to asset path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = '1.1'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
 
 # Precompile additional assets.
