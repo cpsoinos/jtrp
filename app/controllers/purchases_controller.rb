e007c1d61769bc2d51e2d179ee32dfb4a77f92d5
Corey Psoinos <coreypsoinos@gmail.com>
2017-05-14 18:22:13 -0400

    ability to edit purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -14,4 +14,43 @@ class PurchasesController < ApplicationController
     @title = "#{@job.name} - Proposal #{@agreement.proposal.id} - #{@agreement.humanized_agreement_type}"
   end
 
+  def edit
+    @agreement = Agreement.find(params[:id])
+    @account = @agreement.account
+    @job = @agreement.job
+    @items = @agreement.items.where.not(status: "potential").order(:account_item_number)
+    @title = "#{@job.name} - Proposal #{@agreement.proposal.id} - #{@agreement.humanized_agreement_type}"
+  end
+
+  def update
+    @agreement = Agreement.find(params[:id])
+    if params[:agreement][:date]
+      params[:agreement][:date] = DateTime.strptime(params[:agreement][:date], '%m/%d/%Y')
+    end
+    if @agreement.update(agreement_params)
+      notice = 'Purchase order was successfully updated.'
+    else
+      notice = 'Unable to update purchase order.'
+    end
+    redirect_to purchase_path(@agreement), notice: notice
+  end
+
+  def destroy
+    @agreement = Agreement.find(params[:id])
+    if @agreement.destroy
+      notice = "Agreement destroyed"
+      redirect_to purchases_path, notice: notice
+    else
+      notice = "Agreement could not be destroyed"
+      redirect_to :back, notice: notice
+    end
+
+  end
+
+  protected
+
+  def agreement_params
+    params.require(:agreement).permit(:service_charge, :date, :check_number)
+  end
+
 end
