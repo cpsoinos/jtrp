e4ee3b15765afc94ee0fb8db224252ece4417eee
Corey Psoinos <coreypsoinos@gmail.com>
2017-05-14 14:09:02 -0400

    address security concerns and remove unused gems

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,8 +1,8 @@
-WickedPdf.config ||= {}
-WickedPdf.config.merge!({
-  print_media_type: true,
-  no_background: true,
-  page_size: "Letter",
-  no_stop_slow_scripts: true,
-  zoom: 0.95,
-})
+# WickedPdf.config ||= {}
+# WickedPdf.config.merge!({
+#   print_media_type: true,
+#   no_background: true,
+#   page_size: "Letter",
+#   no_stop_slow_scripts: true,
+#   zoom: 0.95,
+# })
