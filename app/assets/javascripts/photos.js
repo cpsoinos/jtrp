d3484539b2a50d35d1e717c21a7efcb52e9cd17d
Corey Psoinos <coreypsoinos@gmail.com>
2017-05-14 11:02:50 -0400

    move widget js to pipeline and add fab to proposal nav

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -2,10 +2,40 @@ $(document).on('turbolinks:load', function() {
 
   // sort photos
   $('#photos').sortable({
-    // handle: '.handle',
     update: function() {
       return $.post($(this).data('update-url'), $(this).sortable('serialize'));
     }
   });
 
+  if (document.getElementById("proposal-photos-form") != undefined) {
+    initPhotoUploadWidget('photos[]', $("#proposal-photos-form"), true);
+  }
+
+  if (document.getElementById("item-photos-form") != undefined) {
+    initPhotoUploadWidget('item[initial_photos][]', $("#item-photos-form"));
+  }
+
 })
+
+function initPhotoUploadWidget(fieldName, $form, submitOnSuccess) {
+  $('#upload_widget_opener').cloudinary_upload_widget(
+    {
+      upload_preset: 'widget',
+      sources: ['local', 'camera'],
+      theme: 'white',
+      button_class: 'btn btn-primary',
+      button_caption: 'Select Images',
+      multiple: true,
+      resource_type: 'image',
+      show_powered_by: false,
+      field_name: fieldName
+    },
+    function(error, result) {
+      if (error) {
+        console.log(error, result)
+      } else if (submitOnSuccess) {
+        $form.submit();
+      }
+    }
+  );
+}
