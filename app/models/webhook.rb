803ff9eab8d8a6d2f26978481a22f2a4d1095cae
Corey Psoinos <coreypsoinos@gmail.com>
2017-06-28 18:29:32 -0400

    don't turn away non-clover or lob webhooks

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -26,6 +26,7 @@ class Webhook < ActiveRecord::Base
   private
 
   def create_webhook_entries
+    return unless remote_entries.present?
     remote_entries.each do |entry|
       Webhooks::Entries::Creator.new(self, entry).create
     end
