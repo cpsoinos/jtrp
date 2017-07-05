991074c82c95e1ffe80382d2a26107d194f107e3
Corey Psoinos <coreypsoinos@gmail.com>
2017-07-05 18:43:52 -0400

    send individual emails for daily summary instead of to team@

--- spec/jobs/daily_summary_email_job_spec.rb
+++ spec/jobs/daily_summary_email_job_spec.rb
@@ -1,19 +1,18 @@
 describe DailySummaryEmailJob do
 
-  let(:user) { create(:internal_user) }
-  let(:notifier) { double("notifier") }
+  let!(:users) { create_list(:internal_user, 3) }
 
   before do
-    Company.jtrp.update_attribute("primary_contact_id", user.id)
-    allow(Notifier).to receive(:send_daily_summary_email).and_return(notifier)
-    allow(notifier).to receive(:deliver_now)
+    Company.jtrp.update_attribute("primary_contact_id", users.first.id)
+    allow(Notifier).to receive_message_chain(:send_daily_summary_email, :deliver_later)
   end
 
   it "perform" do
     DailySummaryEmailJob.perform_later
 
-    expect(Notifier).to have_received(:send_daily_summary_email).with(Company.jtrp.primary_contact)
-    expect(notifier).to have_received(:deliver_now)
+    users.each do |user|
+      expect(Notifier).to have_received(:send_daily_summary_email).with(user)
+    end
   end
 
 end
