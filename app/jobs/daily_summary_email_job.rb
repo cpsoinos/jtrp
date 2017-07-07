61b3e794611fadbe9a3561ce36234e46708dc73f
Corey Psoinos <coreypsoinos@gmail.com>
2017-07-06 23:21:27 -0400

    add admin to daily summary email recipients

--- app/jobs/daily_summary_email_job.rb
+++ app/jobs/daily_summary_email_job.rb
@@ -2,9 +2,15 @@ class DailySummaryEmailJob < ActiveJob::Base
   queue_as :cron
 
   def perform
-    InternalUser.all.each do |user|
+    users.each do |user|
       Notifier.send_daily_summary_email(user).deliver_later
     end
+  end
+
+  private
+
+  def users
+    @_users ||= InternalUser.all + Admin.all
   end
 
 end
