05b102f32e1bf9e8e30f64b0fabaa2da1f083e41
Corey Psoinos <coreypsoinos@gmail.com>
2017-08-05 14:00:20 -0400

    move state machines to modules

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -13,46 +13,6 @@ describe Account do
   let!(:account) { create(:account) }
   it { should validate_presence_of(:status) }
 
-  describe Account, "state machine" do
-
-    it "starts as 'potential'" do
-      expect(Account.new).to be_potential
-    end
-
-    it "transitions 'potential' to 'active'" do
-      account = create(:account)
-      create(:job, :active, account: account)
-      account.mark_active
-
-      expect(account).to be_active
-    end
-
-    it "transitions 'active' to 'completed'" do
-      account = create(:account, :active)
-      create(:job, :completed, account: account)
-      account.mark_inactive
-
-      expect(account).to be_inactive
-    end
-
-    it "transitions 'potential' to 'inactive'" do
-      item = create(:item)
-      account = item.account
-      account.mark_inactive!
-
-      expect(account.reload).to be_inactive
-      expect(item.reload).to be_inactive
-    end
-
-    it "transitions 'inactive' to 'active'" do
-      account = create(:account, :inactive)
-      account.reactivate!
-
-      expect(account).to be_active
-    end
-
-  end
-
   it "primary_contact" do
     account = create(:account, :with_client)
 
