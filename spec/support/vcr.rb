f6266bad8c2262daefb9103cf348b5ed1b1fec01
Corey Psoinos <coreypsoinos@gmail.com>
2017-08-05 15:02:45 -0400

    add capybara server and port to vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   # config.ignore_localhost = true
-  config.ignore_hosts ['localhost', docker_ip]
+  config.ignore_hosts ['localhost', docker_ip, '0.0.0.0']
   config.default_cassette_options = { record: :new_episodes }
   config.configure_rspec_metadata!
 end
