3bf033ef2f4b5d4edc23195b898234f62dd8dc38
Corey Psoinos <coreypsoinos@gmail.com>
2017-08-05 19:56:58 -0400

    ignore port

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,10 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   # config.ignore_localhost = true
-  config.ignore_hosts ['localhost', docker_ip, '0.0.0.0']
+  config.ignore_hosts ['localhost', '127.0.0.1', docker_ip, '0.0.0.0']
+  config.ignore_request do |request|
+    URI(request.uri).port == '3010'
+  end
   config.default_cassette_options = { record: :new_episodes }
   config.configure_rspec_metadata!
 end
