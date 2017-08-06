1b855a9dcfbfa02b71bf23b152c490d5b1c4ed97
Corey Psoinos <coreypsoinos@gmail.com>
2017-08-05 22:06:17 -0400

    set chrome to be headless

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,7 +2,7 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'capybara-screenshot/rspec'
 
-Capybara.javascript_driver = :chrome
+Capybara.javascript_driver = :docker_chrome
 
 # options = {
 #   js_errors: false,
@@ -28,28 +28,49 @@ Capybara.javascript_driver = :chrome
 
 # Capybara.javascript_driver = :chrome
 
+require "selenium/webdriver"
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
+
+
 # use container's shell to find the docker ip address
 docker_ip = %x(/sbin/ip route|awk '/default/ { print $3 }').strip
 
 Capybara.register_driver :docker_chrome do |app|
   Capybara::Selenium::Driver.new(app, {
     browser: :remote,
-    url: "#{ENV['SELENIUM_URL']}/wd/hub",
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome
-    # url: "http://#{docker_ip}:4444/wd/hub"
+    # url: "#{ENV['SELENIUM_URL']}/wd/hub",
+    url: "http://#{docker_ip}:4444/wd/hub"
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+    )
   })
 end
 
-def setup_driver
-  unless ENV['SELENIUM_URL'].nil? || ENV['SELENIUM_URL'].empty?
-    Capybara.current_driver    = :docker_chrome
-    Capybara.javascript_driver = :docker_chrome
-    Capybara.server_port       = 55555
-    Capybara.server_host       = "#{ENV['LOCAL_IP']}"
-    Capybara.app_host          = "http://#{ENV['LOCAL_IP']}:#{Capybara.server_port}"
-  end
-end
-# 
+# def setup_driver
+#   unless ENV['SELENIUM_URL'].nil? || ENV['SELENIUM_URL'].empty?
+#     Capybara.current_driver    = :docker_chrome
+#     Capybara.javascript_driver = :docker_chrome
+#     Capybara.server_port       = 55555
+#     Capybara.server_host       = "#{ENV['LOCAL_IP']}"
+#     Capybara.app_host          = "http://#{ENV['LOCAL_IP']}:#{Capybara.server_port}"
+#   end
+# end
+#
 # Capybara.app_host = "http://#{docker_ip}:3010"
 # Capybara.server_host = '0.0.0.0'
 # Capybara.server_port = '3010'
