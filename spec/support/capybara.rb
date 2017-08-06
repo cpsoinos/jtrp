66c498a1fdfd802fd4297c1476d1954448ab5329
Corey Psoinos <coreypsoinos@gmail.com>
2017-08-05 22:31:30 -0400

    tidy up

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,34 +1,5 @@
 require 'capybara/rspec'
-# require 'capybara/poltergeist'
-# require 'capybara-screenshot/rspec'
-
-# Capybara.javascript_driver = :poltergeist
-#
-# options = {
-#   js_errors: false,
-#   timeout: 180,
-#   phantomjs_logger: StringIO.new,
-#   logger: nil,
-#   extenstions: ["#{Rails.root}/spec/support/phantomjs/disable_animations.js"],
-#   phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes']
-# }
-#
-# Capybara.register_driver(:poltergeist) do |app|
-#   Capybara::Poltergeist::Driver.new app, options
-# end
-#
-# Capybara.register_server(:puma) do |app, port|
-#   require 'rack/handler/puma'
-#   Rack::Handler::Puma.run(app, Port: port)
-# end
-#
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
-
-# Capybara.javascript_driver = :chrome
-
-require "selenium/webdriver"
+require 'selenium/webdriver'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
