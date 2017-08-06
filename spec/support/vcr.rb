6ca1236841490b3af73df54310e640d2b2bdccf6
Corey Psoinos <coreypsoinos@gmail.com>
2017-08-05 20:32:24 -0400

    tweak ignored hosts

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,12 +3,12 @@ docker_ip = %x(/sbin/ip route|awk '/default/ { print $3 }').strip
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.ignore_localhost = true
-  config.ignore_hosts docker_ip
-  # config.ignore_request do |request|
-  #   URI(request.uri).port == '3010'
-  #   URI(request.uri).path =~ /(__inspect__)/
-  # end
+  # config.ignore_localhost = true
+  config.ignore_hosts docker_ip, 'localhost', '127.0.0.1', '0.0.0.0'
+  config.ignore_request do |request|
+    URI(request.uri).port.in?(%w(3030 4444))
+    URI(request.uri).path =~ /(__inspect__)/
+  end
   config.default_cassette_options = { record: :new_episodes }
   config.configure_rspec_metadata!
 end
