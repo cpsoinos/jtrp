c0fd8fa4e2d0e1064418a876dedfc890922f9a42
Corey Psoinos <coreypsoinos@gmail.com>
2017-08-05 20:23:48 -0400

    attempt to roll back to ignoring localhost with docker ip

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,12 +3,12 @@ docker_ip = %x(/sbin/ip route|awk '/default/ { print $3 }').strip
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  # config.ignore_localhost = true
-  config.ignore_hosts ['localhost', '127.0.0.1', docker_ip, '0.0.0.0']
-  config.ignore_request do |request|
-    URI(request.uri).port == '3010'
-    URI(request.uri).path =~ /(__inspect__)/
-  end
+  config.ignore_localhost = true
+  config.ignore_hosts docker_ip
+  # config.ignore_request do |request|
+  #   URI(request.uri).port == '3010'
+  #   URI(request.uri).path =~ /(__inspect__)/
+  # end
   config.default_cassette_options = { record: :new_episodes }
   config.configure_rspec_metadata!
 end
