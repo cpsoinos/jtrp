feea2859a92671575ea4a8cb22ccf3971267dc5e
Corey Psoinos <coreypsoinos@gmail.com>
2017-09-04 16:43:59 -0400

    add docker chrome back to capybara config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -14,4 +14,18 @@ Capybara.register_driver :headless_chrome do |app|
     desired_capabilities: capabilities
 end
 
+# use container's shell to find the docker ip address
+docker_ip = %x(/sbin/ip route|awk '/default/ { print $3 }').strip
+
+Capybara.register_driver :docker_chrome do |app|
+  Capybara::Selenium::Driver.new(app, {
+    browser: :remote,
+    # url: "#{ENV['SELENIUM_URL']}/wd/hub",
+    url: "http://#{docker_ip}:4444/wd/hub",
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+    )
+  })
+end
+
 Capybara.javascript_driver = :headless_chrome
